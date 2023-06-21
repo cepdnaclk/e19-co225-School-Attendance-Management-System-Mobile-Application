7a07d5d50c835db25ffe7e6ef60595e568b19aac
mewanDimalsha <e19111@eng.pdn.ac.lk>
2023-06-21 10:30:04 +0530

    Merge branch 'main' of https://github.com/cepdnaclk/e19-co225-School-Attendance-Management-System-Mobile-Application

---
+++ Attendme2/app/src/main/java/com/example/attendme/LoginActivity.java
@@ -0,0 +1,88 @@
+package com.example.attendme;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText login_email,login_password;
+    private TextView signupRedirectText;
+    private Button login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        login_email = findViewById(R.id.login_email);
+        login_password = findViewById(R.id.login_password);
+        login_button = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = login_email.getText().toString();
+                String pass = login_password.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if (!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                launchMainActivity();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }else {
+                        login_password.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    login_email.setError("Email cannot be empty");
+                }
+                else {
+                    login_email.setError("Please enter valid email");
+                }
+
+            }
+
+        });
+
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignInActivity.class));
+            }
+
+        });
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Optional: finish LoginActivity to prevent going back to it
+    }
+}
